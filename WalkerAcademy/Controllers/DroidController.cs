89e51253a720ac64d0171fff3f0fdaf9a9553c2a
harrdev <jasonharrphx@live.com>
2023-08-20 18:40:35 -0500

    added Droid model, controller, and view

---
+++ WalkerAcademy/Controllers/DroidController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using WalkerAcademy.Models;
+
+namespace WalkerAcademy.Controllers
+{
+    public class DroidController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMemoryCache _cache;
+
+        public DroidController(IHttpClientFactory httpClient, IMemoryCache cache)
+        {
+            _httpClientFactory = httpClient;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            const string cacheKey = "DroidList";
+            if (!_cache.TryGetValue<List<Droid>>(cacheKey, out var allDroids))
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                httpClient.BaseAddress = new Uri("https://starwars-databank-server.vercel.app");
+
+                allDroids = new List<Droid>();
+                string? nextPage = "/api/v1/droids?page=1";
+
+                while (!string.IsNullOrEmpty(nextPage))
+                {
+                    var response = await httpClient.GetAsync(nextPage);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return BadRequest();
+                    }
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    var droidPage = JsonConvert.DeserializeObject<ApiResponse<Droid>>(content);
+
+                    if (droidPage?.Data == null || !droidPage.Data.Any())
+                    {
+                        break;
+                    }
+
+                    allDroids.AddRange(droidPage.Data);
+
+                    nextPage = droidPage.Info.Next;
+                }
+
+                var cacheOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(60) // Cache for 1 hour
+                };
+                _cache.Set(cacheKey, allDroids, cacheOptions);
+            }
+            System.Diagnostics.Debug.WriteLine(allDroids.Count);
+            return View(allDroids);
+        }
+    }
+}
