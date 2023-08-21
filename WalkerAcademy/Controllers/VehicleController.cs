8d32c61f635ffa8f96856742f71d019165902816
harrdev <jasonharrphx@live.com>
2023-08-20 19:03:00 -0500

    added Vehicle model, controller, and view

---
+++ WalkerAcademy/Controllers/VehicleController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using WalkerAcademy.Models;
+
+namespace WalkerAcademy.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMemoryCache _cache;
+
+        public VehicleController(IHttpClientFactory httpClient, IMemoryCache cache)
+        {
+            _httpClientFactory = httpClient;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            const string cacheKey = "VehicleList";
+            if (!_cache.TryGetValue<List<Vehicle>>(cacheKey, out var allVehicles))
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                httpClient.BaseAddress = new Uri("https://starwars-databank-server.vercel.app");
+
+                allVehicles = new List<Vehicle>();
+                string? nextPage = "/api/v1/vehicles?page=1";
+
+                while (!string.IsNullOrEmpty(nextPage))
+                {
+                    var response = await httpClient.GetAsync(nextPage);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return BadRequest();
+                    }
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    var vehiclePage = JsonConvert.DeserializeObject<ApiResponse<Vehicle>>(content);
+
+                    if (vehiclePage?.Data == null || !vehiclePage.Data.Any())
+                    {
+                        break;
+                    }
+
+                    allVehicles.AddRange(vehiclePage.Data);
+
+                    nextPage = vehiclePage.Info.Next;
+                }
+
+                var cacheOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(60) // Cache for 1 hour
+                };
+                _cache.Set(cacheKey, allVehicles, cacheOptions);
+            }
+            System.Diagnostics.Debug.WriteLine(allVehicles.Count);
+            return View(allVehicles);
+        }
+    }
+}
